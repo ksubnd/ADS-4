9417f51e1a4db9f6ea54ff41d9471634e8a50c2c
ksubnd <ksubnd@gmail.com>
2026-04-03 23:21:13 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,52 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+#include <iostream>
+
+int countPairs1(int* arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len / 2 + 1; i++)
+    for (int j = i+1; j < len; j++)
+      if (arr[i] + arr[j] == value)
+        count += 1;
+  return count;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+int countPairs2(int* arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len / 2 + 1; i++)
+    for (int j = len - 1; j > i; j--) {
+      if (arr[i] + arr[j] < value)
+        break;
+      else if (arr[i] + arr[j] == value)
+        count += 1;
+    }
+  return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+int binSearch(int* arr, int value, int l, int r) {
+  if (l > r)
+    return -1;
+  else if (arr[(r + l) / 2] == value)
+    return (r + l) / 2;
+  else if (arr[(r + l) / 2] > value)
+    return binSearch(arr, value, l, (r + l) / 2 - 1);
+  else
+    return binSearch(arr, value, (r + l) / 2 + 1, r);
+}
+int countPairs3(int* arr, int len, int value) {
+  int count = 0;
+  for (int i = 0; i < len / 2 + 1; i++) {
+    int chislo = value - arr[i];
+    int bin = binSearch(arr, chislo, 0, len - 1);
+    if (bin != -1) {
+      int l = bin;
+      while (arr[i] + arr[l] == value) {
+        count += 1;
+        l -= 1;
+      }
+      int r = bin + 1;
+      while (arr[i] + arr[r] == value) {
+        count += 1;
+        r += 1;
+      }
+    }
+  }
+  return count;
 }
